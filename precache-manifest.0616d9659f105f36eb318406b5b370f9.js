45c8a9e5863d87c234369bd32cd427132bae8d6d
bergdavi <berghammer.david@gmail.com>
2020-02-06 21:26:55 +0100

    Added plex gui demo

---
+++ precache-manifest.0616d9659f105f36eb318406b5b370f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcbe2d9566e3cbc5a3d26a5f7cc698b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "387cd470e14ab9605879",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "122ad49c9e7498eb3b05",
+    "url": "/static/css/main.13350641.chunk.css"
+  },
+  {
+    "revision": "387cd470e14ab9605879",
+    "url": "/static/js/2.81a54b46.chunk.js"
+  },
+  {
+    "revision": "fea558630ab7dce5ce9eb6690c151222",
+    "url": "/static/js/2.81a54b46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "122ad49c9e7498eb3b05",
+    "url": "/static/js/main.b2960763.chunk.js"
+  },
+  {
+    "revision": "ed1080afd35291252566",
+    "url": "/static/js/runtime-main.4f83f69a.js"
+  }
+]);
